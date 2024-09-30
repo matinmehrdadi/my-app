7624734f09c5accfaca8212d04310f201bc97cc8
matinmehrdadi <matin.md.2004.9@gmail.com>
2024-09-30 16:41:09 +0330

    90-start

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>about page</h1>
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 export default function Home() {
-  console.log("Excuting...");
+  // console.log("Excuting...");
 
   return (
     <main>
       <h1>welcome to this nextjs course!</h1>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
