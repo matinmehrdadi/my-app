4dcd5f2674a183c7783bb2339456242f4d05d73a
matin.md <matin.md.2004.9@gmail.com>
2024-10-09 00:50:25 +0330

    a

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
-import classes from "../components/main-header.module.css";
+import classes from "../../components/main-header/main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
 export default function MainHeader() {
   return (
     <>
       <MainHeaderBackground />
-      <div className=" backG w-[100%] h-96 text-white px-28 z-10 ">
+      <div className="w-[100%] h-96 text-white px-28 z-10 ">
         <header className="flex text-center items-center content-center justify-between pt-12">
           <div className="flex text-center items-center">
             <Link href="/" className=" flex text-center items-center ">
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,8 @@
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className=" text-center grid pt-28">
-      <h1 className="text-5xl pb-5">Time to get started !</h1>
-      <p>
-        <Link href="/meals">/ Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">/ Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">/ Community</Link>
-      </p>
+    <main>
+
     </main>
   );
 }
